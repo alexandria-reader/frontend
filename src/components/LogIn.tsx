e95059554fbee055a4aa5e0ae69f9b060fc07884
Dana Chen <danaqchen@gmail.com>
2021-12-23 22:46:10 +0100

    log in from api

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,31 +1,25 @@
 import { useContext, useState } from 'react';
 import Nav from './Nav';
 import UserContext from '../contexts/UserContext';
-import { login } from '../utils/login';
+// import { login } from '../utils/login';
 import Login from '../utils/loginForm';
 
 export default function LogIn() {
   const { user, setUser } = useContext(UserContext);
   const [token, setToken] = useState();
-
+  console.log(user);
   return (
     <div>
      <Nav />
-     { token ? '' : (
-       <Login setToken={setToken} />
-     )
+     {/* {console.log(token.username)} */}
+     { token && user
+     // FIXME: token exists but can't seem to get the user object in, also can't parse token object
+     // TODO: loginForm is able to push the data into local storage, but before this page renders
+       ? ('You are logged in')
+       : (
+       <Login setToken={setToken} setUser={setUser}/>
+       )
      }
-
-     <div>{JSON.stringify(user, null, 2)} is logged in</div>
-     <button
-      onClick={async () => {
-        // eslint-disable-next-line @typescript-eslint/no-shadow
-        const user = await login();
-        setUser(user);
-      }}
-      >
-      login
-      </button>
     </div>
   );
 }
--- src/utils/loginForm.js
+++ src/utils/loginForm.js
@@ -1,42 +1,44 @@
-import React, { useState } from "react";
+import { useState } from 'react';
 import axios from 'axios';
 
 const loginUser = async function(credentials) {
-  const userInfo = JSON.stringify(credentials);
-  const request = await axios.post('http://localhost:3000/api/login', userInfo).then(response => response.data.json());
-  
-  return request.data;
-}
+  const request = await axios.post('http://localhost:3000/api/login', credentials).then((response) => response.data);
+  return request;
+};
 
-export default function Login({setToken}) {
-  const [username, setUserName] = useState();
+export default function Login({ setToken, setUser }) {
+  const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const user = {};
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-      const token = await loginUser({
-        username,
-        password
-      });
-      setToken(token);
-  }
+    const token = await loginUser({
+      email,
+      password,
+    });
+    console.log(token);
+    setToken(token);
+    setUser(user);
+    localStorage.setItem('token', JSON.stringify(token));
+  };
 
-  return(
+  return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
       <form onSubmit={handleSubmit}>
         <label>
-          <p>Username</p>
-          <input type="text" onChange={e => setUserName(e.target.value)}/>
+          <p>Email</p>
+          <input type="text" onChange={(e) => setEmail(e.target.value)}/>
         </label>
         <label>
           <p>Password</p>
-          <input type="password" onChange={e => setPassword(e.target.value)}/>
+          <input type="password" onChange={(e) => setPassword(e.target.value)}/>
         </label>
         <div>
           <button type="submit">Submit</button>
         </div>
       </form>
     </div>
-  )
+  );
 }
