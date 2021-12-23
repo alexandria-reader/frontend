542da73bd05d7a1aabaf758b80bcf1b10adb1818
Eamon <eamon0989@gmail.com>
2021-12-23 15:26:24 +0100

    refactored Texts, moved logic to singleText, changed word.state to word.status, split up long lines

--- src/components/SingleText.tsx
+++ src/components/SingleText.tsx
@@ -1,9 +1,15 @@
-import { MarkedWords, Text, UserWord } from '../types';
+import { useState, useEffect } from 'react';
+import {
+  MarkedWords, Text, UserWord, State,
+} from '../types';
+import wordsService from '../services/words';
+import UserInput from './UserInput';
 
 const Word = function ({
   word, status, handleWordClick, getSelection,
 }: { word: string, status: string, handleWordClick: Function, getSelection: Function }) {
-  return <span onMouseUp={(event) => getSelection(event)} onClick={(event) => handleWordClick(event)} className={`${status} word`}>{word}</span>;
+  return <span onMouseUp={(event) => getSelection(event)}
+  onClick={(event) => handleWordClick(event)} className={`${status} word`}>{word}</span>;
 };
 
 const Phrase = function ({
@@ -15,7 +21,9 @@ const Phrase = function ({
   return (
     <span onClick={(event) => handleWordClick(event)} className={`${status} phrase`}>
       {
-        parts.map((word, index, array) => <><Word handleWordClick={handleWordClick} getSelection={getSelection} key={word + index} word={word} status={markedWords[word.toLowerCase()]} />{index === array.length - 1 ? '' : ' '}</>)
+        parts.map((word, index, array) => <><Word handleWordClick={handleWordClick}
+        getSelection={getSelection} key={word + index} word={word}
+        status={markedWords[word.toLowerCase()]} />{index === array.length - 1 ? '' : ' '}</>)
       }
     </span>
   );
@@ -26,9 +34,10 @@ const Paragraph = function({
 }: { paragraph: string, words: UserWord[], handleWordClick: Function, getSelection: Function }) {
   const markedWords: MarkedWords = {};
   // eslint-disable-next-line no-return-assign
-  words.forEach((obj) => markedWords[obj.word] = obj.state);
+  words.forEach((obj) => markedWords[obj.word] = obj.status);
 
-  const phraseFinder = `(${Object.keys(markedWords).filter((key) => key.split(' ').length > 1).join('|')})`;
+  const phraseFinder = `(${Object.keys(markedWords)
+    .filter((key) => key.split(' ').length > 1).join('|')})`;
   const wordFinder = '(?<words>[\\p{Letter}\\p{Mark}\'-]+)';
   const noWordFinder = '(?<nowords>[^\\p{Letter}\\p{Mark}\'-]+)';
 
@@ -46,9 +55,16 @@ const Paragraph = function({
     <p>
       {
         tokens.map((token, index) => {
-          if (phraseFinder !== '()' && token.match(phraseRegExp)) return <Phrase getSelection={getSelection} handleWordClick={handleWordClick} key={token + index} phrase={token} markedWords={markedWords} status={markedWords[token.toLowerCase()]} />;
-          // eslint-disable-next-line max-len
-          if (token.match(wordRegExp)) return <Word getSelection={getSelection} handleWordClick={handleWordClick} key={token + index} word={token} status={markedWords[token.toLowerCase()]} />;
+          if (phraseFinder !== '()' && token.match(phraseRegExp)) {
+            return <Phrase getSelection={getSelection} handleWordClick={handleWordClick}
+            key={token + index} phrase={token} markedWords={markedWords}
+            status={markedWords[token.toLowerCase()]} />;
+          }
+          if (token.match(wordRegExp)) {
+            return <Word getSelection={getSelection}
+            handleWordClick={handleWordClick} key={token + index} word={token}
+            status={markedWords[token.toLowerCase()]} />;
+          }
           return <span key={token + index}>{token}</span>;
         })
       }
@@ -56,20 +72,131 @@ const Paragraph = function({
   );
 };
 
-const SingleTextBody = function ({
-  text, words, handleWordClick, getSelection,
-}: { text: Text, words: UserWord[], handleWordClick: Function, getSelection: Function }) {
+const SingleTextBody = function ({ text }: { text: Text }) {
+  const [words, setWords]: [words: [] | UserWord[], setWords: Function] = useState([]);
+  const [currentWord, setCurrentWord]: [word: null | UserWord, setWord: Function] = useState(null);
+
+  const getUserWordsFromServer = function() {
+    wordsService.getWordsFromText(String(text.id), text.languageId)
+      .then((userWords) => setWords(userWords));
+  };
+
+  const callServer = () => getUserWordsFromServer();
+  useEffect(callServer, []);
+
+  const setWordStatus = function(status: State, word: UserWord) {
+    const newWord = { ...word };
+    newWord.status = status;
+
+    setCurrentWord(newWord);
+
+    const updatedWords = [...words.filter((wordObj) => wordObj.word.toLowerCase()
+      !== newWord.word.toLowerCase()), newWord];
+    setWords(updatedWords);
+  };
+
+  const handleWordClick = function(event: React.MouseEvent<HTMLSpanElement, MouseEvent>) {
+    const input = event.target as HTMLElement;
+    const word = input.textContent || '';
+
+    const wordObj = words.filter((arrWordObj) => arrWordObj.word.toLowerCase()
+      === word.toLowerCase());
+
+    if (wordObj.length > 0) {
+      const wordObject = wordObj[0];
+
+      if (wordObject.status === undefined || wordObject.status === 'undefined') {
+        wordObject.status = 'learning';
+      }
+
+      const updatedWords = [...words.filter((arrWordObj) => arrWordObj.word.toLowerCase()
+        !== word.toLowerCase()), wordObject];
+      setWords(updatedWords);
+      setCurrentWord(wordObject);
+    } else {
+      const newWordObj = {
+        word: `${word.toLowerCase()}`, status: 'learning', translations: [], contexts: [],
+      };
+      setCurrentWord(newWordObj);
+      const updatedWords = [...words, newWordObj];
+      setWords(updatedWords);
+    }
+  };
+
+  const getSelection = function(_event: { target: { textContent: unknown; }; }) {
+    // todo: check interaction between this and cycleState
+    // fix bug where if a user selects backwards, first and last words are swapped
+    // gets the selection string
+    const selection = window.getSelection();
+    if (selection !== null && selection.toString()) {
+      const selectedString = selection.toString();
+
+      const startNode = selection.anchorNode;
+      const endNode = selection.focusNode;
+      const stringArray = selectedString.split(' ');
+
+      // ensures the first and last words are whole words
+      let startWord = '';
+      let endWord = '';
+
+      if (startNode && startNode.textContent) {
+        startWord = startNode.textContent;
+        if (stringArray[0] && startWord) {
+          stringArray[0] = startWord;
+        }
+      }
+
+      if (endNode && endNode.textContent) {
+        endWord = endNode.textContent;
+        if (stringArray[stringArray.length - 1] && endWord) {
+          stringArray[stringArray.length - 1] = endWord;
+        }
+      }
+
+      const newPhrase = stringArray.join(' ').trim().split('.')[0];
+
+      // adds the phrase to words with state: learning
+      const newWordObj = {
+        word: `${newPhrase.toLowerCase()}`, state: 'learning', translations: [], contexts: [],
+      };
+      setCurrentWord(newWordObj);
+
+      if (words.filter((wordObj) => wordObj.word.toLowerCase()
+        === newWordObj.word.toLowerCase()).length === 0) {
+        const updatedWords = [...words, newWordObj];
+        setWords(updatedWords);
+      }
+    }
+  };
+
+  const handleTranslation = function(
+    event: React.FormEvent<HTMLFormElement>,
+    translation: string,
+    word: UserWord,
+  ) {
+    event.preventDefault();
+
+    const newWord = { ...word };
+    newWord.translations = [...word.translations, translation];
+    setCurrentWord(newWord);
+
+    const updatedWords = [...words.filter((wordObj) => wordObj.word.toLowerCase()
+      !== newWord.word.toLowerCase()), newWord];
+    setWords(updatedWords);
+  };
+
   const textBody = text.body;
   const paragraphs = textBody?.split('\n');
 
   return (
-    <div className="text-div">
-      {
-        // eslint-disable-next-line max-len
-        paragraphs.map((paragraph, index) => <Paragraph getSelection={getSelection} handleWordClick={handleWordClick} key={index} paragraph={paragraph} words={words} />)
-      }
-      {/* <UserInput /> */}
-    </div>
+    <>
+      <div className="text-div">
+        {paragraphs.map((paragraph, index) => <Paragraph getSelection={getSelection}
+          handleWordClick={handleWordClick} key={index} paragraph={paragraph} words={words} />)}
+      </div>
+        <UserInput word={currentWord} setWordStatus={setWordStatus}
+          handleTranslation={handleTranslation} />
+    </>
   );
 };
 
--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -3,6 +3,7 @@ import { UserWord } from '../types';
 
 // eslint-disable-next-line max-len
 const Translation = function({ word, handleTranslation }: { word: UserWord | null, handleTranslation: Function }) {
+  // if a translation is not set, the state should be restored to 'undefined'
   const [translation, setTranslation] = useState('');
   const handleInput = function(event: ChangeEvent<HTMLInputElement>) {
     setTranslation(event.target.value);
@@ -10,7 +11,8 @@ const Translation = function({ word, handleTranslation }: { word: UserWord | nul
 
   return (
     <div className="translation-div">
-      {word && word?.translations.length > 0 && <p>Current translation: {word?.translations}</p>}
+      {word && word?.translations.length > 0
+      && <p>Current translation: {word?.translations.join(', ')}</p>}
       <form onSubmit={(event) => {
         handleTranslation(event, translation, word);
         setTranslation('');
@@ -27,28 +29,28 @@ const Translation = function({ word, handleTranslation }: { word: UserWord | nul
 };
 
 // eslint-disable-next-line max-len
-const ChangeState = function({ word, setStateTo }: { word: UserWord | null, setStateTo: Function }) {
+const ChangeState = function({ word, setWordStatus }: { word: UserWord | null, setWordStatus: Function }) {
   return (
     <div className="change-state-div">
-      {word && <p>Current state: {word.state}</p>}
+      {word && <p>Current state: {word.status}</p>}
       <div className="word-state-toolbar">
-        <button onClick={() => setStateTo('learning', word)} type={'button'}>Learning</button>
-        <button onClick={() => setStateTo('familiar', word)} type={'button'}>Familiar</button>
-        <button onClick={() => setStateTo('known', word)} type={'button'}>Known</button>
-        <button onClick={() => setStateTo('undefined', word)} type={'button'}>Ignore</button>
+        <button onClick={() => setWordStatus('learning', word)} type={'button'}>Learning</button>
+        <button onClick={() => setWordStatus('familiar', word)} type={'button'}>Familiar</button>
+        <button onClick={() => setWordStatus('known', word)} type={'button'}>Known</button>
+        <button onClick={() => setWordStatus('undefined', word)} type={'button'}>Ignore</button>
       </div>
     </div>
   );
 };
 
 // eslint-disable-next-line max-len
-const UserInput = function({ word, setStateTo, handleTranslation }: { word: UserWord | null, setStateTo: Function, handleTranslation: Function }) {
+const UserInput = function({ word, setWordStatus, handleTranslation }: { word: UserWord | null, setWordStatus: Function, handleTranslation: Function }) {
   return (
     <div className="user-input-div">
       {word && <p>Selected word: {word.word}</p>}
       {!word && <p>Select a word</p>}
       <Translation handleTranslation={handleTranslation} word={word} />
-      <ChangeState word={word} setStateTo={setStateTo} />
+      <ChangeState word={word} setWordStatus={setWordStatus} />
     </div>
   );
 };
