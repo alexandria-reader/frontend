c16ce57fa2db3a7a493459605266fc176936845f
Dana Chen <danaqchen@gmail.com>
2021-12-28 14:51:01 +0100

    languages sent to backend working

--- src/services/users.ts
+++ src/services/users.ts
@@ -5,7 +5,6 @@ const baseUrl = 'http://localhost:3000/api/users';
 
 const addUser = async function(newUser: User) {
   const request = await axios.post(`${baseUrl}`, newUser);
-
   const user: User = request.data;
   return user;
 };
