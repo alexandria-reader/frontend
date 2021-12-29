08ff78513066b4cd8c02a8acaa64fc9485664648
Eamon <eamon0989@gmail.com>
2021-12-29 17:06:56 +0100

    when a user clicks on the text div, but not on a word, the current word is deselected (and if it doesnt have a translation, removed)

--- src/components/texts/Body-Paragraph.tsx
+++ src/components/texts/Body-Paragraph.tsx
@@ -1,11 +1,13 @@
 import {
-  selector, useRecoilValue,
+  selector, useRecoilState, useRecoilValue,
 } from 'recoil';
 
 import { Word, Phrase } from './Phrase-Word';
 
 import {
+  currentwordState,
   markedwordsState,
+  userwordsState,
 } from '../../states/recoil-states';
 
 const phrasesState = selector({
@@ -45,13 +47,33 @@ const Paragraph = function({ paragraph }: { paragraph: string }) {
   );
 };
 
+const isElement = function(element: Element | EventTarget): element is Element {
+  return (element as Element).nodeName !== undefined;
+};
 
 const TextBody = function ({ title, textBody }: { title: string, textBody: string }) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [currentWord, setCurrentWord] = useRecoilState(currentwordState);
+  const [userWords, setUserWords] = useRecoilState(userwordsState);
   const paragraphs = textBody.split('\n');
+
+  const removeUnusedWord = function(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    const { target }: { target: Element | EventTarget } = event;
+    // if a user clicks empty space inside the text div, the current word is removed
+    // if that word did not have a translation (or id) it is removed from userWords,
+    // removing the highlight
+    if (isElement(target) && target.nodeName !== 'SPAN') {
+      setCurrentWord(null);
+
+      const updatedWords = [...userWords
+        .filter((wordObj) => wordObj.id !== undefined)];
+      setUserWords(updatedWords);
+    }
+  };
 
   return (
     <>
-      <div className="text-div">
+      <div onClick={(event) => removeUnusedWord(event)} className="text-div">
         { // title needs to be mapped so users can click on words in it
         }
         <h1>{title}</h1>
