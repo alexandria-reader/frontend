a4f6800ce186d808dd104eeb36c935b90c7ba6be
Eamon <eamon0989@gmail.com>
2021-12-29 12:30:53 +0100

    added token to getTextById route. Fixed bug where a page reload on a text resulted in loss of context. Uesr can now navigate directly to a text, e.g. `/texts/4`

--- src/components/texts/SingleText.tsx
+++ src/components/texts/SingleText.tsx
@@ -1,28 +1,40 @@
-/* eslint-disable max-len */
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Nav from '../Nav';
 import TranslationInput from './TranslationInput';
 import TextBody from './Body-Paragraph';
-
 import wordsService from '../../services/words';
-
+import textsService from '../../services/texts';
 import { userwordsState, currenttextState, currentwordState } from '../../states/recoil-states';
 
 const SingleText = function () {
-  const currentText = useRecoilValue(currenttextState);
+  const [currentText, setCurrentText] = useRecoilState(currenttextState);
   const currentWord = useRecoilValue(currentwordState);
   const setUserWords = useSetRecoilState(userwordsState);
+  const params = useParams();
+
+  const fetchUserwords = async function() {
+    if (currentText) {
+      const userWordsResponse = await wordsService
+        .getUserwordsInText(String(currentText.id), currentText.languageId);
+      setUserWords(userWordsResponse);
+    }
+  };
+
+  const getTextById = async function() {
+    if (params.textId) {
+      const text = await textsService.getTextById(params.textId);
+      setCurrentText(text);
+    }
+  };
 
   useEffect(() => {
-    const fetchUserwords = async function() {
-      if (currentText) {
-        const userWordsResponse = await wordsService.getUserwordsInText(String(currentText.id), currentText.languageId);
-        setUserWords(userWordsResponse);
-      }
-    };
-
-    fetchUserwords();
+    if (currentText) {
+      fetchUserwords();
+    } else {
+      getTextById();
+    }
   }, [currentText]);
 
   if (currentText) {
@@ -44,7 +56,8 @@ const SingleText = function () {
     <div className='Text-page'>
       <Nav />
       <div className='text-div'>
-        <h1>No text found.</h1>
+        <h1>No text found. (This sometimes appears when a text is being loaded from the server,
+          it will be replaced later by a placeholder)</h1>
       </div>
     </div>
   );
