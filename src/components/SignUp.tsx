d78a00c13bc854bfc959e78d998b3f7af056c1bc
Dana Chen <danaqchen@gmail.com>
2021-12-29 21:01:39 +0100

    user logged in and directed to texts after sign up

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,16 +1,20 @@
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { useRecoilState } from 'recoil';
 import userServices from '../services/users';
 import languageServices from '../services/languages';
+import loginService from '../services/login';
 import { languagesState } from '../states/recoil-states';
 import Nav from './Nav';
+import { User } from '../types';
 
 export default function SignUp() {
+  const navigate = useNavigate();
   const [languages, setLanguages] = useRecoilState(languagesState);
   const {
     register, formState: { errors }, handleSubmit, setError,
-  } = useForm();
+  } = useForm({ mode: 'onChange' });
 
   const getLanguageListFromServer = async function() {
     const dbLanguages = await languageServices.getAllLanguages();
@@ -32,7 +36,7 @@ export default function SignUp() {
 
          <form onSubmit={handleSubmit(async (data) => {
            if (data.currentKnownLanguageId === data.currentLearnLanguageId) {
-             setError('languages', { type: 'languages', message: 'Learning language cannot be the same as known language' });
+             setError('languages', { type: 'languages', message: ' Learning language cannot be the same as known language' });
              return;
            }
            const user = {
@@ -43,12 +47,22 @@ export default function SignUp() {
              currentLearnLanguageId: data.currentLearnLanguageId,
            };
            const response = await userServices.addUser(user);
+           console.log(response);
+           //  Need to make the following happen only after the user has been added
            if (typeof response === 'string') {
              setError('emailTaken', { type: 'email', message: response });
+           } else if (response.status === 201) {
+             console.log('response.status is 201');
+             const loggedInUser: User = await loginService.loginUser({
+               email: user.email,
+               password: user.password,
+             });
+             localStorage.setItem('user', JSON.stringify(loggedInUser));
+             navigate('/texts');
            }
          })}>
            <label className="label">Name</label>
-           <input {...register('username', { required: true, minLength: 3 })} className="input" type="text" />
+           <input {...register('username', { required: true, minLength: 3, maxLength: 20 })} className="input" type="text" />
            {errors.username?.type === 'required' && ' Please enter a user name.'}
            {errors.username?.type === 'minLength' && ' Name should have a mininum of 3 characters.'}
            <br></br>
