e855ea3956e43437af62a13dcab5f35ae11ee21d
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-12-31 18:51:49 +0100

    first try at Words page

--- src/components/Words.tsx
+++ src/components/Words.tsx
@@ -1,14 +1,93 @@
-import getToken from '../utils/getToken';
+import { useEffect } from 'react';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { userwordsState, currentUserLanguagesState } from '../states/recoil-states';
 
-export default function Words() {
-  const tokenObj = getToken();
+import wordsService from '../services/words';
+
+const WordTable = function () {
+  const userwords = useRecoilValue(userwordsState);
 
   return (
-    <div>
-     Home
-     <div> { tokenObj
-       ? (`${tokenObj.username} is logged in`) : 'You are not logged in.'
-     } </div>
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Word
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Translation
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Context
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {userwords.map((userword) => (
+                  <tr key={userword.word}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {userword.word}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                        {userword.status}
+                      </span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                      {userword.translations[0].translation}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {userword.translations[0].context}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+const Words = function() {
+  const setUserWords = useSetRecoilState(userwordsState);
+  const currentUserLanguages = useRecoilValue(currentUserLanguagesState);
+
+  const fetchUserwords = async function() {
+    if (currentUserLanguages) {
+      const userWordsResponse = await wordsService
+        .getUserwordsByLanguage(currentUserLanguages?.currentLearnLanguageId);
+      setUserWords(userWordsResponse);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserwords();
+  }, [currentUserLanguages]);
+
+  return (
+    <WordTable />
+  );
+};
+
+export default Words;
---
+++ src/services/host.ts
@@ -0,0 +1,1 @@
+export default process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://alexandria-reader-staging.herokuapp.com';
