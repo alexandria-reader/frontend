624bf66ae8ae31871cfba11e0ae2df86b15cb4ac
Dana Chen <danaqchen@gmail.com>
2021-12-31 10:49:40 +0100

    landing page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import getToken from '../utils/getToken';
+import Benefits from './home/Benefits';
+import HowItWorks from './home/HowItWorks';
 
 export default function Home() {
   const tokenObj = getToken();
+  const navigate = useNavigate();
 
   return (
     <div className='home-page'>
-     Home
      <div> { tokenObj
-       ? (`${tokenObj.username} is logged in`) : 'You are not logged in.'
+       ? navigate('/texts')
+       : <div>
+          <Benefits />
+          <HowItWorks />
+        </div>
      } </div>
-     <p>Insert all the awesome information about the app here.</p>
     </div>
   );
 }
