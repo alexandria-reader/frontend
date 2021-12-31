4b139972230a962f644f916a3b0f05233e83f47d
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-12-31 15:23:58 +0100

    console.log  in login

--- src/services/login.ts
+++ src/services/login.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { LoginDetails } from '../types';
 import dbHost from './dbHost';
 
+console.log(process.env.DB_HOST);
+
 const baseUrl = `${dbHost}/api/login`;
 
 const loginUser = async function(credentials: LoginDetails) {
