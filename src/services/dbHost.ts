ae73cf2349b9334e6f13dae5ef05a40860bdafc2
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-12-31 16:00:53 +0100

    REACT_APP_ env vars

--- src/services/dbHost.ts
+++ src/services/dbHost.ts
@@ -1,1 +1,1 @@
-export default process.env.DB_HOST ? process.env.DB_HOST : 'http://localhost:3000';
+export default process.env.REACT_APP_DB_HOST ? process.env.REAC_APP_DB_HOST : 'http://localhost:3000';
