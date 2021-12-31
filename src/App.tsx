538ca49c5ac8324dfa2df74859fa0a5c94ae98a4
Eamon <eamon0989@gmail.com>
2021-12-31 16:53:15 +0100

    Got NavBar working on all pages. It automatically changes when logging in or out.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import { Outlet } from 'react-router';
 import './App.css';
-import Home from './components/Home';
 import Nav from './components/Nav';
-import LoggedOutNav from './components/LoggedOutNav';
-import getToken from './utils/getToken';
 
 function App() {
-  const tokenObj = getToken();
-
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      {tokenObj
-        ? <>
           <Nav />
           <main className='container mx-auto'>
             <Outlet />
           </main>
-        </>
-        : <>
-        <LoggedOutNav />
-      <Home />
-    </>}
   </div>
   );
 }
--- src/components/LoggedOutNav.tsx
+++ src/components/LoggedOutNav.tsx
@@ -1,7 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable max-len */
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { Disclosure } from '@headlessui/react';
 import { NavLink, useLocation } from 'react-router-dom';
 import getToken from '../utils/getToken';
 
@@ -26,8 +23,6 @@ export default function Example() {
         <>
           <div className="max-w-7xl mx-auto px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
-              {/* <div className="absolute inset-y-0 left-0 flex items-center">
-              </div> */}
               <div className="flex-1 flex items-stretch justify-between">
                 <div className="flex-shrink-0 flex items-center">
                 <NavLink to={'/'}>
@@ -61,35 +56,11 @@ export default function Example() {
                     </Disclosure.Button>
                     </NavLink>;
                     })}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-
-          {/* This is the mobile dropdown menu */}
-          {/* <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 bg-gray-800 pt-2 pb-3 space-y-1">
-            {navigation.map((item) => {
-              const isActive = useLocation().pathname === item.href;
-
-              return <NavLink to={`${item.href}`}>
-                        <Disclosure.Button
-                          key={item.name}
-                          as="div"
-                          // href={item.href}
-                          className={classNames(
-                            isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'block px-3 py-2 rounded-md text-base font-medium',
-                          )}
-                          aria-current={isActive ? 'page' : undefined}
-                        >
-                      {item.name}
-                      </Disclosure.Button>
-                    </NavLink>;
-            })}
-            </div>
-          </Disclosure.Panel> */}
         </>
       )}
     </Disclosure>
