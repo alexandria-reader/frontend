16a8cb9a722fbaad540d230a2455458e8d785ad6
Eamon <eamon0989@gmail.com>
2022-01-01 17:53:28 +0100

    Improved mobile layout

--- src/components/texts/TranslationInput.tsx
+++ src/components/texts/TranslationInput.tsx
@@ -11,10 +11,45 @@ import {
   userwordsState, currentwordState, currenttextState, currentUserLanguagesState,
 } from '../../states/recoil-states';
 
+const ChangeStatus = function({ word }: { word: UserWord | null }) {
+  const [userWords, setUserWords] = useRecoilState(userwordsState);
+  const setCurrentWord = useSetRecoilState(currentwordState);
+  const setWordStatus = function(status: Status, userWord: UserWord) {
+    const newUserWord = { ...userWord };
+    newUserWord.status = status;
+
+    if (newUserWord.id) {
+      // return word object rather than just status from backend
+      wordsService.updateStatus(newUserWord);
+    }
+    setCurrentWord(newUserWord);
+
+    const updatedWords = [...userWords.filter((wordObj: UserWord) => wordObj.word.toLowerCase()
+      !== newUserWord.word.toLowerCase()), newUserWord];
+    setUserWords(updatedWords);
+  };
+
+  const wordStatusToolbar = word
+    ? <div className="flex flex-row justify-center overflow-visible">
+        <button className='bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-3 rounded my-4' onClick={() => setWordStatus('learning', word)} type={'button'}>Learning</button>
+        <button className='bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-3 rounded my-4' onClick={() => setWordStatus('familiar', word)} type={'button'}>Familiar</button>
+        <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-3 rounded my-4' onClick={() => setWordStatus('learned', word)} type={'button'}>Learned</button>
+        <button className='bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-3 rounded my-4' onClick={() => setWordStatus(undefined, word)} type={'button'}>Ignore</button>
+      </div>
+    : '';
+
+  return (
+    <div className="">
+      {word && <p className='text-xl'>Current status: {word.status}</p>}
+      {wordStatusToolbar}
+    </div>
+  );
+};
+
 const DictionaryIframe = function({ url }: { url: string }) {
   return (
     <>
-      <iframe width="350" height="400"
+      <iframe width="320" height="350"
         src={url}>
       </iframe>
     </>
@@ -152,77 +187,53 @@ const TranslationComponent = function({ word }: { word: UserWord | null }) {
         </form>
         {/* <div> */}
         <div className='flex flex-col justify-center'>
-          {showDictionary && <><button onClick={() => setShowDictionary(false)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-1'>Close Dictionary</button>
+          {showDictionary && <><button onClick={() => setShowDictionary(false)} className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded my-1'>Close Dictionary</button>
           <DictionaryIframe url={`https://www.wordreference.com/${currentText?.languageId}${currentUserLanguages?.currentKnownLanguageId}/${currentWord.word}`} /></>}
           {!showDictionary && <><p>View word in dictionary:</p>
           <button onClick={() => setShowDictionary(true)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-1'>WordReference</button>
           <button onClick={() => window.open(`https://www.deepl.com/translator#${currentText?.languageId}/${currentUserLanguages?.currentKnownLanguageId}/${currentWord.word}/`, 'DeepL', 'left=100,top=100,width=650,height=550')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-1'>DeepL (Popup)</button>
-          <button onClick={() => window.open(`https://www.wordreference.com/${currentText?.languageId}${currentUserLanguages?.currentKnownLanguageId}/${currentWord.word}`, 'WordReference', 'left=100,top=100,width=350,height=550')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-1'>WordReference (Popup)</button>
-          <button onClick={() => window.open(`https://translate.google.com/?sl=${currentText?.languageId}&tl=${currentUserLanguages?.currentKnownLanguageId}&text=${currentWord.word}%0A&op=translate/`, 'Google Translate', 'left=100,top=100,width=350,height=550')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-1'>Google Translate (Popup)</button>
+          {/* <button onClick={() => window.open(`https://www.wordreference.com/${currentText?.languageId}${currentUserLanguages?.currentKnownLanguageId}/${currentWord.word}`, 'WordReference', 'left=100,top=100,width=350,height=550')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-1'>WordReference (Popup)</button>
+          <button onClick={() => window.open(`https://translate.google.com/?sl=${currentText?.languageId}&tl=${currentUserLanguages?.currentKnownLanguageId}&text=${currentWord.word}%0A&op=translate/`, 'Google Translate', 'left=100,top=100,width=350,height=550')} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-1'>Google Translate (Popup)</button> */}
           </>}
         </div>
       {/* </div> */}
+        {!showDictionary && <ChangeStatus word={word} />
+}
       </div></>}
     </div>
   );
 };
 
-const ChangeStatus = function({ word }: { word: UserWord | null }) {
-  const [userWords, setUserWords] = useRecoilState(userwordsState);
-  const setCurrentWord = useSetRecoilState(currentwordState);
-  const setWordStatus = function(status: Status, userWord: UserWord) {
-    const newUserWord = { ...userWord };
-    newUserWord.status = status;
-
-    if (newUserWord.id) {
-      // return word object rather than just status from backend
-      wordsService.updateStatus(newUserWord);
-    }
-    setCurrentWord(newUserWord);
-
-    const updatedWords = [...userWords.filter((wordObj: UserWord) => wordObj.word.toLowerCase()
-      !== newUserWord.word.toLowerCase()), newUserWord];
-    setUserWords(updatedWords);
-  };
-
-  const wordStatusToolbar = word
-    ? <div className="word-status-toolbar">
-        <button className='bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-3 rounded my-4' onClick={() => setWordStatus('learning', word)} type={'button'}>Learning</button>
-        <button className='bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-3 rounded my-4' onClick={() => setWordStatus('familiar', word)} type={'button'}>Familiar</button>
-        <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-3 rounded my-4' onClick={() => setWordStatus('learned', word)} type={'button'}>Learned</button>
-        <button className='bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-3 rounded my-4' onClick={() => setWordStatus(undefined, word)} type={'button'}>Ignore</button>
-      </div>
-    : '';
-
-  return (
-    <div className="change-status-div">
-      {word && <p className='text-xl'>Current status: {word.status}</p>}
-      {wordStatusToolbar}
-    </div>
-  );
-};
-
-
 const TranslationInput = function({ word }: { word: UserWord | null }) {
-  console.log(word);
-  return (
-    <>
-      <div className='bg-white shadow sm:rounded-lg sm:px-6 md:flex flex-col m-4 md:col-start-3 min-w-min md:col-span-1 hidden'>
-        <h2 className='ml-2 text-3xl font-medium text-gray-900 my-4'>{word ? `${word.word}` : 'Select a word'}</h2>
-        <TranslationComponent word={word} />
-        <ChangeStatus word={word} />
-      </div>
+  if (window.innerWidth > 768) {
+    return (
+      <>
+        <div className='bg-white shadow sm:rounded-lg sm:px-6 md:flex flex-col m-4 md:col-start-3 min-w-min md:col-span-1 hidden'>
+          <h2 className='ml-2 text-3xl font-medium text-gray-900 my-4'>{word ? `${word.word}` : 'Select a word'}</h2>
+          <TranslationComponent word={word} />
+          {/* <ChangeStatus word={word} /> */}
+        </div>
+      </>
+    );
+  }
+
+  // blur background when modal?
+  if (word) {
+    return (
       <div className='sm:hidden m-2 fixed inset-0 flex items-end pointer-events-none sm:p-6 sm:items-start'>
-        <div className='w-full p-4 overflow-scroll pointer-events-auto flex flex-col items-center shadow-lg rounded-lg space-y-4 sm:items-end bg-gray-400'>
-      {/* <div className='sm:hidden fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start'>
-        <div className='w-full flex flex-col items-center shadow-lg rounded-lg space-y-4 sm:items-end bg-gray-400'> */}
+        <div className='w-full p-4 overflow-scroll pointer-events-auto flex flex-col items-center shadow-lg rounded-lg space-y-4 sm:items-end bg-gray-300'>
           <div className='w-full overflow-scroll'>
+            <h2 className='text-3xl font-medium text-gray-900 mb-2'>{word ? `${word.word}` : 'Select a word'}</h2>
             <TranslationComponent word={word} />
-            <ChangeStatus word={word} />
+            {/* <ChangeStatus word={word} /> */}
           </div>
         </div>
       </div>
-    </>
+    );
+  }
+
+  return (
+    <></>
   );
 };
 
