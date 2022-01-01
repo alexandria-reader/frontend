f8d48a59bbb4bd359f7812299e360da2e0a215ca
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2022-01-01 11:44:44 +0100

    run dev from heroku

--- src/services/host.ts
+++ src/services/host.ts
@@ -1,1 +1,3 @@
-export default process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://alexandria-reader-staging.herokuapp.com';
+export default process.env.NODE_ENV === 'development'
+  ? (process.env.REACT_APP_DB_HOST || 'http://localhost:3000')
+  : 'https://alexandria-reader-staging.herokuapp.com';
