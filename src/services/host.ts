5d9cb3b8c660088fad31391cc928f4997903fb46
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2022-01-01 11:57:53 +0100

    dev and production DB nowfrom  env

--- src/services/host.ts
+++ src/services/host.ts
@@ -1,3 +1,2 @@
 export default process.env.NODE_ENV === 'development'
-  ? (process.env.REACT_APP_DB_HOST || 'http://localhost:3000')
-  : 'https://alexandria-reader-staging.herokuapp.com';
+  ? process.env.REACT_APP_DEV_DB : process.env.REACT_APP_PROD_DB;
