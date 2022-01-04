fc76bc469df690b3dc76db3bb69341e650da55ad
Dana Chen <danaqchen@gmail.com>
2022-01-04 14:53:34 +0100

    styling update landing page

--- src/components/home/Benefits.tsx
+++ src/components/home/Benefits.tsx
@@ -5,18 +5,17 @@ export default function Benefits() {
         <div className="pt-32 pb-12 md:pt-20 md:pb-4">
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="md:text-8xl font-extrabold leading-tighter tracking-normal mb-16" data-aos="zoom-y-out">Learn languages by<br></br><span className="bg-clip-text text-transparent bg-gradient-to-r from-[#167cbd] to-[#6a007b]">reading what you enjoy</span></h1>
+            <h1 className="text-4xl md:text-8xl sm:text-6xl font-extrabold leading-tighter tracking-normal mb-16" data-aos="zoom-y-out">Learn languages by<br></br><span className="bg-clip-text text-transparent bg-gradient-to-r from-[#167cbd] to-[#6a007b]">reading what you enjoy</span></h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-16 tracking-normal" data-aos="zoom-y-out" data-aos-delay="150">Upload texts. Highlight words you want to learn. Review regularly.</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:inline-flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-10 rounded mx-10"><a href="/login">Sign in</a></button>
-                <br></br>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-10 rounded mx-10 my-3"><a href="/login">Sign in</a></button>
                 </div>
                 <div>
                   {/* <a className="btn text-white bg-gray-900 hover:bg-gray-800
                   w-full sm:w-auto sm:ml-4" href="#0">Sign up</a> */}
-                  <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-10 rounded"><a href="/signup">Sign up</a></button>
+                  <button className="bg-gray-900 hover:bg-gray-800 text-white py-2 px-10 rounded mx-10 my-3"><a href="/signup">Sign up</a></button>
                 </div>
               </div>
             </div>
