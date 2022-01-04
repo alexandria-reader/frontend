962384380f0a19ab62f4b2aa3dea0079f16b46ed
Eamon <eamon0989@gmail.com>
2022-01-04 16:35:17 +0100

    Fixed bug where existing phrases couldnt be selected

--- src/components/texts/Body-Paragraph.tsx
+++ src/components/texts/Body-Paragraph.tsx
@@ -2,6 +2,7 @@ import {
   selector, useRecoilState, useRecoilValue, useSetRecoilState,
 } from 'recoil';
 import { useState, useEffect } from 'react';
+// import { textContent } from 'domutils';
 import {
   currentwordContextState, currentwordState, markedwordsState, userwordsState,
 } from '../../states/recoil-states';
@@ -78,9 +79,10 @@ const TextBody = function ({ title, textBody }: { title: string, textBody: strin
     return (element as Element).nodeName !== undefined;
   };
 
-  const removeUnusedWord = function(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+  const removeUnusedWordOrGetPhrase = function(
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) {
     const { target }: { target: Element | EventTarget } = event;
-    // event.preventDefault
     // if a user clicks empty space inside the text div, the current word is removed
     // if that word did not have a translation (or id) it is removed from userWords
     if (!window.getSelection()?.toString() && isElement(target) && target.nodeName !== 'SPAN') {
@@ -90,13 +92,24 @@ const TextBody = function ({ title, textBody }: { title: string, textBody: strin
         .filter((wordObj) => wordObj.id !== undefined)];
       setUserWords(updatedWords);
       setCurrentWordContext(null);
+    } else if (isElement(target) && target.nodeName === 'SPAN' && target?.textContent) {
+      const text = target?.textContent?.split(' ').filter(Boolean);
+
+      // if user clicks on a span containing words and a space, it's a phrase
+      if (text.length > 1) {
+        const current = userWords.filter((wordObj) => wordObj.word === target.textContent);
+        if (current.length === 1) {
+          setCurrentWord(current[0]);
+        }
+      }
     }
+
     window.getSelection()?.removeAllRanges();
   };
 
   return (
     <>
-      <div onMouseUp={(event) => removeUnusedWord(event)} className={`container mx-auto p-4 m-4 md:col-span-2 md:col-start-1 bg-white px-4 py-5 shadow sm:rounded-lg sm:px-6 ${currentWord && window.innerWidth < 760 ? 'blur-sm bg-gray-300' : ''}`}>
+      <div onMouseUp={(event) => removeUnusedWordOrGetPhrase(event)} className={`container mx-auto p-4 m-4 md:col-span-2 md:col-start-1 bg-white px-4 py-5 shadow sm:rounded-lg sm:px-6 ${currentWord && window.innerWidth < 760 ? 'blur-sm bg-gray-300' : ''}`}>
         <h1 className='font-bold text-3xl mb-2'>{title}</h1>
           {paragraphs.map((paragraph, index) => <><div className='mb-3'><Paragraph key={index} paragraph={paragraph} /></div></>)}
       </div>
