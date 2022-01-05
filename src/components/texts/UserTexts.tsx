a049d3f57d54192374bce82b6bf4d9891d1262db
Eamon <eamon0989@gmail.com>
2022-01-05 15:34:59 +0100

    Removed alert that stopped build from compiling on Netlify

--- src/components/texts/UserTexts.tsx
+++ src/components/texts/UserTexts.tsx
@@ -62,7 +62,7 @@ const TextEditDropdown = function() {
                             href=""
                             className={`block px-4 py-2 text-sm text-gray-700 ${active ? 'bg-gray-100' : ''}`}
                           >
-                            <div className='flex flex-row justify-between m-2' onClick={() => alert('Are you sure you want to delete this text? (Deletion not ready)')}>
+                            <div className='flex flex-row justify-between m-2'>
                               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                               </svg>
