d89a4d1cac78f14bccb8037861d95350ecad6745
Eamon <eamon0989@gmail.com>
2022-01-05 11:25:20 +0100

    Fixed a bug where setting menu was hidden behind translation div

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -202,7 +202,7 @@ export default function Navbar() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="origin-top-right z-10 absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <a
