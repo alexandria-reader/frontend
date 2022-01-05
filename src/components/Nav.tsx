843dec3517b8f6103c2de51b1baf2ab9bce7fe3a
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2022-01-05 11:59:11 +0100

    turned settings link into NavLink

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -205,12 +205,17 @@ export default function Navbar() {
                     <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="settings"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
+                          <NavLink to='/settings'>
+                            <Disclosure.Button
+                              key='Settings'
+                              as="a"
+                              href='/settings'
+                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                              aria-current={active ? 'page' : undefined}
+                            >
                             Settings
-                          </a>
+                          </Disclosure.Button>
+                        </NavLink>
                         )}
                       </Menu.Item>
                       <Menu.Item>
