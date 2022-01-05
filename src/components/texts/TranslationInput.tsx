9e681a071bc55c4a1ec0ccf4987b5662f023da2a
Eamon <eamon0989@gmail.com>
2022-01-05 08:28:05 +0100

    Fix layout problems on iPad and other devices for the single page text view, translation element now has a fixed width on larger devices

--- src/components/texts/TranslationInput.tsx
+++ src/components/texts/TranslationInput.tsx
@@ -50,11 +50,11 @@ const ChangeStatus = function({ word }: { word: UserWord | null }) {
 
 const DictionaryIframe = function({ url }: { url: string }) {
   return (
-    <>
-      <iframe width="320" height="350"
+    <div className=''>
+      <iframe title='Wordreference dictionary' className='' width="320" height="350"
         src={url}>
       </iframe>
-    </>
+    </div>
   );
 };
 
@@ -227,7 +227,7 @@ const TranslationInput = function({ word }: { word: UserWord | null }) {
   if (window.innerWidth > 768) {
     return (
       <>
-        <div className='bg-white shadow sm:rounded-lg sm:px-6 py-4 md:flex flex-col m-4 md:col-start-3 min-w-min md:col-span-1 hidden'>
+        <div className='bg-white shadow sm:rounded-lg sm:px-6 py-4 md:flex flex-col md:col-start-2 w-[368px] md:col-span-1 hidden'>
           {word && <div className='flex flex-row items-center'>
             <svg onClick={() => speak()} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" />
