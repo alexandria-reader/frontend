b84cae2ec6ce0dc3a6e5eda986619e48dadd761a
Eamon <eamon0989@gmail.com>
2022-01-05 15:27:55 +0100

    Added dropdown menu on user text items with edit and delete button (functionality not yet implemented)

--- src/components/texts/UserTexts.tsx
+++ src/components/texts/UserTexts.tsx
@@ -1,14 +1,82 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable max-len */
-import { useState, useEffect, FormEvent } from 'react';
+import {
+  useState, useEffect, FormEvent, Fragment,
+} from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { Menu, Transition } from '@headlessui/react';
 import {
   textlistState, currenttextState, userState, currentwordState,
 } from '../../states/recoil-states';
 
 import textsService from '../../services/texts';
 import { Text } from '../../types';
+import logOut from '../../utils/logOut';
+
+const TextEditDropdown = function() {
+  const openOptionsMenu = function () {
+
+  };
+
+  return (
+    <>
+      <Menu as="div" className="flex m-3 w-5">
+                  <div>
+                    <Menu.Button className="flex text-sm rounded-full">
+                      <span className="sr-only">Open user menu</span>
+                      <svg onClick={() => openOptionsMenu()} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                      </svg>
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="origin-top-right z-10 absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href=""
+                            className={ `block px-4 py-2 text-sm text-gray-700 ${active ? 'bg-gray-100' : ''}` }
+                          >
+                            <div className='flex flex-row justify-between m-2'>
+                              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                              </svg>
+                              Edit
+                            </div>
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href=""
+                            className={`block px-4 py-2 text-sm text-gray-700 ${active ? 'bg-gray-100' : ''}`}
+                          >
+                            <div className='flex flex-row justify-between m-2' onClick={() => alert('Are you sure you want to delete this text? (Deletion not ready)')}>
+                              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                              </svg>
+                              Delete
+                            </div>
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+    </>
+  );
+};
 
 const IndividualText = function({ text }: { text: Text }) {
   const setCurrentText = useSetRecoilState(currenttextState);
@@ -24,10 +92,6 @@ const IndividualText = function({ text }: { text: Text }) {
       setTextList(updatedTextList);
       await textsService.removeTextFromServer(id);
     }
-  };
-
-  const openOptionsMenu = function () {
-
   };
 
   useEffect(() => {
@@ -55,14 +119,14 @@ const IndividualText = function({ text }: { text: Text }) {
           </div>
         </Link>
         {/* <div> */}
-          <div className='flex m-3 w-5'>
-            <svg onClick={() => openOptionsMenu()} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          {/* <div className='flex m-3 w-5'> */}
+            {/* <svg onClick={() => openOptionsMenu()} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-            </svg>
+            </svg> */}
+            <TextEditDropdown />
           {/* </div> */}
-        </div>
+        {/* </div> */}
       </div>
-
       {/* <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded' onClick={() => removeTextFromServer(text.id)}>Delete</button> */}
     </li>
   );
