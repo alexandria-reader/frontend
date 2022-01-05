0720c5133f481d2c44e3ff4d3b34a98dd7ba1479
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2022-01-05 22:38:33 +0100

    switched from userState to token check in PrivateRoute

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,12 +1,10 @@
 import { Navigate, useLocation } from 'react-router';
-import { useRecoilValue } from 'recoil';
-import { userState } from '../states/recoil-states';
+import getToken from '../utils/getToken';
 
 export default function RequireAuth({ children }: { children: JSX.Element }) {
-  const user = useRecoilValue(userState);
   const location = useLocation();
 
-  if (!user) {
+  if (!getToken()) {
     return <Navigate to='/login' state={{ from: location }} replace />;
   }
 
