28e950e1849b8161500d31c6bd783c43f84e6e91
Eamon <eamon0989@gmail.com>
2022-01-06 12:31:03 +0100

    Removed unnecessary icons from buttons

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -2,7 +2,7 @@
 import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
-import { LockClosedIcon } from '@heroicons/react/solid';
+// import { LockClosedIcon } from '@heroicons/react/solid';
 import { userState } from '../states/recoil-states';
 import loginService from '../services/login';
 import { LoggedInUser } from '../types';
@@ -113,9 +113,9 @@ export default function LoginForm({ from }: { from: string }) {
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                {/* <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
-                </span>
+                </span> */}
                 Sign in
               </button>
             </div>
--- src/components/texts/UserTexts.tsx
+++ src/components/texts/UserTexts.tsx
@@ -40,7 +40,7 @@ const IndividualText = function({ text }: { text: Text }) {
       <div className='flex flex-row justify-between group-hover:shadow-md'>
         <Link className='w-full' key={text.id + text.body.slice(0, 7)} to={`/texts/${text.id}`}>
           <div onClick={(_event) => setCurrentText(text)} className='flex items-center p-6 space-x-6'>
-            <div className='flex justify-center items-center p-4 rounded-full flex-shrink-0 bg-blue-500'>
+            <div className='flex justify-center items-center p-4 rounded-full flex-shrink-0 bg-indigo-500'>
             <svg className="w-7 h-7 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
             </svg>
