66c061f941941a875c42f3e3e918acc81736779e
Eamon <eamon0989@gmail.com>
2022-01-07 17:56:36 +0100

    Added hero mobile responsiveness

--- src/components/home/Benefits.tsx
+++ src/components/home/Benefits.tsx
@@ -8,17 +8,17 @@ export default function Benefits() {
   return (
     <>
       {/* Hero content */}
-      <div className="hero h-screen px-20 py-10">
+      <div className="hero h-screen px-4 py-10 md:px-20 md:py-10">
         <div className="items-center h-full flex flex-col md:flex-row ">
         {/* Section header */}
           <div className=" flex-1 xl:py-28 flex justify-between h-max flex-col gap-6">
-            <h1 className="text-3xl xl:text-6xl lg:text-5xl md:text-4xl sm:text-6xl font-extrabold leading-tighter tracking-normal" data-aos="zoom-y-out">Learn languages by<br></br>
+            <h1 className="text-4xl text-center md:text-left xl:text-6xl lg:text-5xl md:text-4xl sm:text-6xl font-extrabold leading-tighter tracking-normal" data-aos="zoom-y-out">Learn languages by<br></br>
               <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#167cbd] to-[#6a007b]">reading what you enjoy</span>
             </h1>
             <div className="max-w-3xl">
-              <p className="text-xl text-gray-600 tracking-normal" data-aos="zoom-y-out" data-aos-delay="150">Upload texts. Highlight words you want to learn. Review regularly.</p>
+              <p className="text-xl text-center md:text-left text-gray-600 tracking-normal" data-aos="zoom-y-out" data-aos-delay="150">Upload texts. Highlight words you want to learn. Review regularly.</p>
             </div>
-            <div className="max-w-xs sm:max-w-none flex flex-row" data-aos="zoom-y-out" data-aos-delay="300">
+            <div className="flex justify-center flex-row" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a href="/login"><button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 lg:text-xl lg:font-bold  rounded text-white px-4 sm:px-10 border border-indigo-700 py-2 sm:py-4 text-sm">Get started</button></a>
                 </div>
