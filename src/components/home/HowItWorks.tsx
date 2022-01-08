6fa449308d2399f43f695943b7edf60c4686146c
Dana Chen <danaqchen@gmail.com>
2022-01-08 17:34:56 +0100

    update icons

--- src/components/home/HowItWorks.tsx
+++ src/components/home/HowItWorks.tsx
@@ -1,6 +1,10 @@
 /* eslint-disable max-len */
 
 // const logo = require('../../assets/logo/logo-light.png');
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCloudUploadAlt, faSearch, faAdjust, faSave, faCheck, faCompass,
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function HowItWorks() {
   return (
@@ -19,8 +23,9 @@ export default function HowItWorks() {
           <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
 
             {/* 1st item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl py-8">
+              <FontAwesomeIcon icon={faCloudUploadAlt} size='3x' style={{ color: '#0084c7' }}/>
+              {/* <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-sky-600" width="64" height="64" rx="32" />
                   <g strokeWidth="2">
@@ -30,14 +35,14 @@ export default function HowItWorks() {
                     <path className="stroke-current text-white" strokeLinecap="square" d="M41.143 29.714l3.428-3.428-3.428-3.429" />
                   </g>
                 </g>
-              </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Upload text</h4>
+              </svg> */}
+              <h4 className="text-xl font-bold leading-snug tracking-tight mt-3 mb-1">Upload text</h4>
               <p className="text-gray-600 text-center">Add a piece of text you are interested in reading, in the language you are actively learning. For example, a piece of newspaper article, or a story.</p>
             </div>
 
             {/* 2nd item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl py-8">
+              {/* <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-sky-600" width="64" height="64" rx="32" />
                   <g strokeWidth="2" transform="translate(19.429 20.571)">
@@ -45,15 +50,16 @@ export default function HowItWorks() {
                     <path className="stroke-current text-white" d="M19.153 23.267c3.59-2.213 5.99-6.169 5.99-10.696C25.143 5.63 19.514 0 12.57 0 5.63 0 0 5.629 0 12.571c0 4.527 2.4 8.483 5.99 10.696" />
                     <path className="stroke-current text-sky-300" d="M16.161 18.406a6.848 6.848 0 003.268-5.835 6.857 6.857 0 00-6.858-6.857 6.857 6.857 0 00-6.857 6.857 6.848 6.848 0 003.268 5.835" />
                   </g>
-                </g>
-              </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Look up unfamiliar words</h4>
+                </g> */}
+                 <FontAwesomeIcon icon={faSearch} size='3x' style={{ color: '#0084c7' }}/>
+              {/* </svg> */}
+              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 mt-3">Look up unfamiliar words</h4>
               <p className="text-gray-600 text-center">Go through text and click on words you don't know, or are not sure about. Multiple dictionaries are available for translation.</p>
             </div>
 
             {/* 3rd item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl py-8">
+              {/* <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-sky-600" width="64" height="64" rx="32" />
                   <g strokeLinecap="square" strokeWidth="2">
@@ -61,14 +67,15 @@ export default function HowItWorks() {
                     <path className="stroke-current text-white" d="M19.429 25.143A6.857 6.857 0 0126.286 32v1.189L28 37.143l-1.714.571V40A2.286 2.286 0 0124 42.286h-2.286v2.285M44.571 25.143A6.857 6.857 0 0037.714 32v1.189L36 37.143l1.714.571V40A2.286 2.286 0 0040 42.286h2.286v2.285" />
                   </g>
                 </g>
-              </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Set learned status</h4>
+              </svg> */}
+              <FontAwesomeIcon icon={faAdjust} size='3x' style={{ color: '#0084c7' }}/>
+              <h4 className="text-xl font-bold leading-snug tracking-tight mt-3 mb-1">Set learned status</h4>
               <p className="text-gray-600 text-center">Inititally marked words are marked with a "learned status" of "Learning". But feel free to adjust it based on your level of familiarity with the word.</p>
             </div>
 
             {/* 4th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl py-8">
+              {/* <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-sky-600" width="64" height="64" rx="32" />
                   <g transform="translate(22.857 19.429)" strokeWidth="2">
@@ -78,14 +85,15 @@ export default function HowItWorks() {
                     <circle className="stroke-current text-sky-300" strokeLinecap="square" cx="12.571" cy="12.571" r="3.429" />
                   </g>
                 </g>
-              </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Apply translation in context</h4>
+              </svg> */}
+              <FontAwesomeIcon icon={faSave} size='3x' style={{ color: '#0084c7' }}/>
+              <h4 className="text-xl font-bold leading-snug tracking-tight mt-3 mb-1">Apply translation in context</h4>
               <p className="text-gray-600 text-center">Based on surrounding context, add the translation that fit best. You can always add another translation for the same word based on a different context later.</p>
             </div>
 
             {/* 5th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl py-8">
+              {/* <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-sky-600" width="64" height="64" rx="32" />
                   <g strokeLinecap="square" strokeWidth="2">
@@ -93,14 +101,15 @@ export default function HowItWorks() {
                     <path className="stroke-current text-sky-300" d="M38.858 26.993l6.397 1.73-4.473 16.549-13.24-3.58" />
                   </g>
                 </g>
-              </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Adjust learned status</h4>
+              </svg> */}
+              <FontAwesomeIcon icon={faCheck} size='3x' style={{ color: '#0084c7' }}/>
+              <h4 className="text-xl font-bold leading-snug tracking-tight mt-3 mb-1">Adjust learned status</h4>
               <p className="text-gray-600 text-center">If previously marked words already exist in your account, they are shown based on "learned status". Adjust status based on your level of familiarity with the word.</p>
             </div>
 
             {/* 6th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl py-8">
+              {/* <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-sky-600" width="64" height="64" rx="32" />
                   <g strokeWidth="2">
@@ -110,8 +119,9 @@ export default function HowItWorks() {
                     <path className="stroke-current text-sky-300" d="M21.714 40h4.572M24 37.714v4.572M37.714 24h4.572M40 21.714v4.572" strokeLinecap="square" />
                   </g>
                 </g>
-              </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Review word list</h4>
+              </svg> */}
+              <FontAwesomeIcon icon={faCompass} size='3x' style={{ color: '#0084c7' }}/>
+              <h4 className="text-xl font-bold leading-snug tracking-tight mt-3 mb-1">Review word list</h4>
               <p className="text-gray-600 text-center">Marked words are available for review at your convenience. Words are presented with translation and surrounding context to ensure maximum retention.</p>
             </div>
 
