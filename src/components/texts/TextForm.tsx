bf86fe2412c4bcbd82294eac354a8a5ae04d9408
Eamon <eamon0989@gmail.com>
2022-01-15 09:28:49 +0100

    Improved layout and UX of url parsing form. Added more url verification before sending it to the backend.

--- src/components/texts/TextForm.tsx
+++ src/components/texts/TextForm.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import { useState, useEffect, FormEvent } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useRecoilState, useRecoilValue } from 'recoil';
@@ -12,6 +11,8 @@ const TextForm = function() {
   const [newTextBody, setNewTextBody] = useState('');
   const [newTextTitle, setNewTextTitle] = useState('');
   const [newTextURL, setNewTextURL] = useState('');
+  const [warningMessage, setWarningMessage] = useState('');
+  const [showURLWarning, setShowURLWarning] = useState(false);
   const [newTextExtractionURL, setNewTextExtractionURL] = useState('');
   const [textToEdit, setTextToEdit] = useRecoilState(textToEditState);
   const navigate = useNavigate();
@@ -39,6 +40,7 @@ const TextForm = function() {
 
   const submitText = async function(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
     const newTextObj: Text = {
       languageId: user?.learnLanguageId || '',
       title: newTextTitle,
@@ -59,21 +61,27 @@ const TextForm = function() {
   const extractTextFromURL = async function(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    const textObject: ArticleData | null = await urlService.postURL(newTextExtractionURL);
+    if (newTextExtractionURL.startsWith('http')) {
+      setShowURLWarning(false);
+      const textObject: ArticleData | null = await urlService.postURL(newTextExtractionURL);
 
-    if (textObject?.content) {
-      const body = textObject.content;
-      const tempDiv = document.createElement('div');
-      tempDiv.innerHTML = body;
-      const { textContent } = tempDiv;
+      if (textObject?.content) {
+        const body = textObject.content;
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = body;
+        const { textContent } = tempDiv;
 
-      setNewTextBody(textContent || '');
-      setNewTextTitle(textObject.title);
-      setNewTextURL(textObject.url);
-      setNewTextExtractionURL('');
-      // alert('Text extraction successful. Please edit the text if necessary and click save.');
+        setNewTextBody(textContent || '');
+        setNewTextTitle(textObject.title);
+        setNewTextURL(textObject.url);
+        setNewTextExtractionURL('');
+      } else {
+        setWarningMessage('Text extraction failed. Please try another link or copy the text manually');
+        setShowURLWarning(true);
+      }
     } else {
-      alert('Text extraction failed. Please try another link or copy the text manually');
+      setWarningMessage('URL must start with http.');
+      setShowURLWarning(true);
     }
   };
 
@@ -138,7 +146,7 @@ const TextForm = function() {
               submitText(event);
             }
           }}>
-            <div className="shadow sm:rounded-md sm:overflow-hidden">
+            <div className="sm:rounded-md sm:overflow-hidden">
               <div className="px-4 bg-secondary space-y-6 sm:px-6 ">
                 <div className="grid grid-cols-3 gap-6">
                   <div className="col-span-3 sm:col-span-2">
@@ -151,7 +159,7 @@ const TextForm = function() {
                         placeholder='Title'
                         name='title'
                         required
-                        className="focus:ring-sky-500 bg-tertiary dark:border-transparent focus:border-sky-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
+                        className="focus:ring-0 bg-tertiary dark:border-transparent focus:border-sky-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
                         value={newTextTitle}
                         onChange={(e) => setNewTextTitle(e.target.value)}
                       />
@@ -168,34 +176,11 @@ const TextForm = function() {
                       id="about"
                       name="about"
                       rows={10}
-                      className="shadow-sm bg-tertiary dark:border-transparent focus:ring-sky-500 focus:border-sky-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      className="shadow-sm bg-tertiary dark:border-transparent focus:ring-0 focus:border-sky-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
                       placeholder="Paste your text here"
-                      defaultValue={''}
                       value={newTextBody}
                       onChange={(e) => setNewTextBody(e.target.value)}
                   />
-                  </div>
-                </div>
-
-                <div className="grid grid-cols-3 gap-6">
-                  <div className="col-span-3 sm:col-span-2">
-                    <label htmlFor="text-url" className="block text-sm font-medium text-six">
-                      URL
-                    </label>
-                    <div className="mt-1 flex rounded-md shadow-sm">
-                      <span className="inline-flex items-center px-3 dark:border-transparent rounded-l-md border border-r-0 border-gray-300 bg-four text-gray-500 text-sm">
-                        http://
-                      </span>
-                      <input
-                        type="text"
-                        name="text-url"
-                        id="text-url"
-                        value={newTextURL}
-                        onChange={(e) => setNewTextURL(e.target.value)}
-                        className="focus:ring-sky-500 dark:border-transparent bg-tertiary focus:border-sky-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
-                        placeholder="www.example.com"
-                      />
-                    </div>
                   </div>
                 </div>
               </div>
@@ -203,7 +188,7 @@ const TextForm = function() {
               <div className="px-4 py-3 flex justify-end bg-secondary text-right sm:px-6">
                 <button
                   type="submit"
-                  className="inline-flex mx-2 justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-sky-600 hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+                  className={`inline-flex mx-2 ${newTextBody && newTextTitle ? 'bg-sky-600 text-white border-0 focus:ring-sky-500 hover:bg-sky-500' : 'bg-gray-300 dark:bg-gray-600 text-gray-400 pointer-events-none'} justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2`}
                 >
                   Save
                 </button>
@@ -219,59 +204,49 @@ const TextForm = function() {
           </form>
         </div>
 
+        <div className="md:col-span-3 md:gap-6 md:grid md:grid-cols-3 border-t-gray-500">
         {!textToEdit && <div className="md:col-span-1 mt-6 sm:mt-6">
           <div className="px-4 sm:px-0">
             <h3 className="text-lg font-medium leading-6 text-tertiary">{'BETA feature: Extract text from URL'}</h3>
             <p className="mt-1 text-sm text-secondary">
-            {'Automatically extract text. May not work on every website.'}
+            {'Automatically extract text and title. May not work on every website.'}
             </p>
           </div>
         </div>}
         {!textToEdit && <div className="mt-6 md:mt-6 md:col-span-2">
           <form action="#" method="POST" onSubmit={(event) => extractTextFromURL(event)}>
-            <div className="shadow sm:rounded-md sm:overflow-hidden">
+            <div className=" sm:rounded-md sm:overflow-hidden">
               <div className="px-4 bg-secondary space-y-6 sm:px-6">
-                <div className="grid grid-cols-3 gap-6">
+                <div className="">
                   <div className="col-span-3 sm:col-span-2">
                     <label htmlFor="text-url" className="block text-sm font-medium text-six">
-                      Paste a URL:
+                      Source URL:
                     </label>
                     <div className="mt-1 flex rounded-md shadow-sm">
-                      <span className="inline-flex items-center px-3 dark:border-transparent rounded-l-md border border-r-0 border-gray-300 bg-four text-gray-500 text-sm">
-                        http://
-                      </span>
                       <input
                         type="text"
                         name="text-url"
                         id="text-url"
                         value={newTextExtractionURL}
                         onChange={(e) => setNewTextExtractionURL(e.target.value)}
-                        className="focus:ring-sky-500 dark:border-transparent bg-tertiary focus:border-sky-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
-                        placeholder="www.example.com"
+                        className="focus:ring-0 focus:border-sky-500 focus:border dark:border-transparent bg-tertiary flex-1 block w-full rounded-l-md sm:text-sm border-gray-300"
+                        placeholder="https://www.example.com/article"
                       />
+                      <button
+                        type="submit"
+                        className={`inline-flex focus:ring-0 justify-center py-2 px-4 border border-l-0 border-gray-300 dark:border-transparent shadow-sm text-sm font-medium rounded-r-md text-white ${newTextExtractionURL ? 'bg-sky-600 text-white border-1 border-sky-600 focus:ring-sky-600 hover:bg-sky-600' : 'bg-gray-300 dark:bg-gray-600 text-gray-400 pointer-events-none'} focus:outline-none focus:ring-2 focus:ring-offset-2 `}
+                      >
+                        Extract
+                      </button>
                     </div>
+                    {showURLWarning && <p className='text-sm text-red-500'>{warningMessage}</p>}
                   </div>
                 </div>
-              </div>
-
-              <div className="px-4 py-3 flex justify-end bg-secondary text-right sm:px-6">
-                <button
-                  type="submit"
-                  className="inline-flex mx-2 justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-sky-600 hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
-                >
-                  Extract
-                </button>
-                <NavLink to={'/texts'} onClick={() => cancelButton()}>
-                  <button
-                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-fuchsia-800 hover:bg-fuchsia-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-fuchsia-500"
-                  >
-                    Cancel
-                  </button>
-                </NavLink>
               </div>
             </div>
           </form>
         </div>}
+        </div>
       </div>
     </>
   );
