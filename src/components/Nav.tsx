34b40af034359514372fe3212ce41692109e8979
Eamon <eamon0989@gmail.com>
2022-01-15 09:41:13 +0100

    removed ring from settings dropbown menu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -235,9 +235,9 @@ export default function Navbar() {
                 </Menu>
 
                 {/* User profile picture dropdown menu */}
-                <Menu as="div" className="hidden sm:block sm:ml-3 relative">
+                <Menu as="div" className="hidden rounded-md sm:block sm:ml-3 relative">
                   <div>
-                    <Menu.Button className="bg-gray-500 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-200 focus:ring-white">
+                    <Menu.Button className="bg-gray-500 flex text-sm rounded-full focus:outline-none focus:ring-0">
                       <span className="sr-only">Open user menu</span>
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-black click-user" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
