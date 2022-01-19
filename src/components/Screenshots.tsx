440f8bcc4e13e038077821e2316bcf61b194a827
Eamon <eamon0989@gmail.com>
2022-01-19 09:16:08 +0100

    Refactored screenshots

--- src/components/Screenshots.tsx
+++ src/components/Screenshots.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const lightDarkGif = require('../assets/light-dark.gif');
 const setTranslationGif = require('../assets/set-translation.gif');
 const phraseSelectionGif = require('../assets/phrase-selection.gif');
@@ -6,67 +7,41 @@ const settingsPasswordGif = require('../assets/setting_pw.gif');
 const settingsLangGif = require('../assets/setting_lang.gif');
 const signUpGif = require('../assets/sign-up.gif');
 
+const screenshots = [
+  { heading: 'Sign Up', gif: signUpGif },
+  { heading: 'Add translation', gif: setTranslationGif },
+  { heading: 'Select phrases', gif: phraseSelectionGif },
+  { heading: 'Update user name and email in Settings', gif: settingsNameGif },
+  { heading: 'Update password in Settings', gif: settingsPasswordGif },
+  { heading: 'Update user languages in Settings', gif: settingsLangGif },
+  { heading: 'Light and dark mode', gif: lightDarkGif },
+];
+
+const Screenshot = function({ heading, gif }: { heading: string, gif: string }) {
+  return (
+    <div className="dark:bg-gray-800 rounded-md">
+      <div className="text-center justify-center pt-20 pb-10">
+        <p className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
+          {heading}
+        </p>
+        <div className="mr-8 w-full text-3xl font-extrabold leading-9">
+          <img alt="profile" src={gif} className="text-gray-600 dark:text-white w-full text-center text-lg md:text-3xl"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default function Screenshots() {
   return (
-    <div className="dark:bg-gray-800 mx-auto p-8 w-2/3">
-        <p className="text-3xl font-extrabold m-auto text-center pt-20 pb-20">How Alexandria works
-        </p>
-        <div className="justify-center text-center pb-10">
-          <h2 className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
-            Sign Up
-          </h2>
-          <div className="text-3xl font-extrabold leading-9">
-            <img alt="profile" src={signUpGif} className="text-gray-600 dark:text-white w-full justify-center text-lg md:text-3xl"/>
-          </div>
-        </div>
-        <div className="text-center justify-center pt-20 pb-10">
-          <h2 className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
-            Add translation
-          </h2>
-          <div className="mr-8 w-full text-3xl font-extrabold leading-9">
-            <img alt="profile" src={setTranslationGif} className="text-gray-600 dark:text-white w-full text-center text-lg md:text-3xl"/>
-          </div>
-        </div>
-        <div className="text-center justify-center pt-20 pb-10">
-          <h2 className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
-          Select phrases
-          </h2>
-          <div className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
-            <img alt="profile" src={phraseSelectionGif} className="text-gray-600 dark:text-white w-full m-auto text-center text-lg md:text-3xl"/>
-          </div>
-        </div>
-        <div className="text-center justify-center pt-20 pb-10">
-          <h2 className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
-            Update user name and email in Settings
-          </h2>
-          <div className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
-            <img alt="profile" src={settingsNameGif} className="text-gray-600 dark:text-white w-full m-auto text-center text-lg md:text-3xl"/>
-          </div>
-        </div>
-        <div className="text-center justify-center pt-20 pb-10">
-          <h2 className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
-          Update password in Settings
-          </h2>
-          <div className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
-            <img alt="profile" src={settingsPasswordGif} className="text-gray-600 dark:text-white w-full m-auto text-center text-lg md:text-3xl"/>
-          </div>
-        </div>
-        <div className="text-center justify-center pt-20 pb-10">
-          <h2 className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
-            Update user languages in Settings
-          </h2>
-          <div className="mr-8 w-full text-3xl font-extrabold leading-9">
-            <img alt="profile" src={settingsLangGif} className="text-gray-600 dark:text-white w-full m-auto text-center text-lg md:text-3xl"/>
-          </div>
-        </div>
-        <div className="text-center justify-center pt-20 pb-10">
-          <h2 className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
-            Light and dark mode
-          </h2>
-          <div className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
-            <img alt="profile" src={lightDarkGif} className="text-gray-600 dark:text-white w-full m-auto text-center text-lg md:text-3xl"/>
-          </div>
-        </div>
+    <>
+    <div className=" py-8 mx-auto p-8 w-2/3">
+      <h1 className="text-3xl font-extrabold m-auto text-center pt-20 pb-20">
+        How Alexandria works
+      </h1>
+      {screenshots.map((screenshot) => <Screenshot key={screenshot.heading} heading={screenshot.heading} gif={screenshot.gif} />)}
+
     </div>
+    </>
   );
 }
