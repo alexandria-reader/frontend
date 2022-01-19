84834c3b04c17cc0e2decbe5b32637e144de5c33
Eamon <eamon0989@gmail.com>
2022-01-19 14:50:19 +0100

    Fixed on hover for translation x in dark mode

--- src/components/texts/TranslationInput.tsx
+++ src/components/texts/TranslationInput.tsx
@@ -410,7 +410,7 @@ const TranslationInput = function({ word }: { word: UserWord | null }) {
               </svg>
               <h2 className='text-3xl font-medium text-tertiary mb-2 '>{word ? `${word.word}` : 'Select a word'}</h2>
             </div>
-            <div id='close-modal-div' onClick={(event) => closeModal(event)} className='flex flex-row items-center darkhover:bg-gray-700 hover:bg-gray-200 rounded-md'>
+            <div id='close-modal-div' onClick={(event) => closeModal(event)} className='flex flex-row items-center dark:hover:bg-gray-700 hover:bg-gray-200 rounded-md'>
               <svg id='close-modal' xmlns="http://www.w3.org/2000/svg" className="h-11 w-11 p-2 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
               </svg>
