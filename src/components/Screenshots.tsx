61fbc379c345eafb8c7ad6361e528502b814cda7
Eamon <eamon0989@gmail.com>
2022-01-19 09:26:32 +0100

    Refactored How to layout

--- src/components/Screenshots.tsx
+++ src/components/Screenshots.tsx
@@ -19,12 +19,12 @@ const screenshots = [
 
 const Screenshot = function({ heading, gif }: { heading: string, gif: string }) {
   return (
-    <div className="dark:bg-gray-800 rounded-md">
-      <div className="text-center justify-center pt-20 pb-10">
-        <p className="mr-8 w-full text-3xl font-extrabold leading-9 pb-10">
+    <div className="dark:bg-gray-800 bg-white shadow-lg rounded-md p-6">
+      <div className="text-center justify-center flex flex-col gap-6">
+        <p className="mr-8 w-full text-2xl sm:text-3xl font-extrabold leading-9">
           {heading}
         </p>
-        <div className="mr-8 w-full text-3xl font-extrabold leading-9">
+        <div className="w-full text-3xl font-extrabold leading-9">
           <img alt="profile" src={gif} className="text-gray-600 dark:text-white w-full text-center text-lg md:text-3xl"/>
         </div>
       </div>
@@ -35,12 +35,13 @@ const Screenshot = function({ heading, gif }: { heading: string, gif: string }) 
 export default function Screenshots() {
   return (
     <>
-    <div className=" py-8 mx-auto p-8 w-2/3">
-      <h1 className="text-3xl font-extrabold m-auto text-center pt-20 pb-20">
+    <div className=" py-8 mx-auto p-8 flex flex-col gap-10">
+      <h1 className=" text-4xl sm:text-5xl text-center leading-tighter dark:border-b-gray-700 dark:border-b w-full tracking-normal font-extrabold m-auto py-10">
         How Alexandria works
       </h1>
-      {screenshots.map((screenshot) => <Screenshot key={screenshot.heading} heading={screenshot.heading} gif={screenshot.gif} />)}
-
+      {/* <div className=""> */}
+        {screenshots.map((screenshot) => <Screenshot key={screenshot.heading} heading={screenshot.heading} gif={screenshot.gif} />)}
+      {/* </div> */}
     </div>
     </>
   );
