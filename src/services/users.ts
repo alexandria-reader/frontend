24fdcfc662e5220ba7d364c7d13adaf60bfc0e37
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2022-01-20 20:06:56 +0100

    service changes to correspond with back en clean up

--- src/services/users.ts
+++ src/services/users.ts
@@ -6,27 +6,6 @@ import host from './host';
 
 const baseUrl = `${host}/api/users`;
 
-const addUser = async function(newUser: User) {
-  const response = await axios.post(`${baseUrl}/`, newUser)
-    .then((res) => res)
-    .catch((error) => {
-      const { message } = error.response.data.error;
-      return message;
-    });
-  return response;
-};
-
-const setUserLanguages = async function(knownLanguageId: string, learnLanguageId: string) {
-  const token = getToken();
-
-  const response = await axios.put(`${baseUrl}/set-languages`, { knownLanguageId, learnLanguageId }, {
-    headers: { Authorization: `bearer ${token}` },
-  });
-
-  // const updatedUser: SanitizedUser = response.data;
-  return response.data;
-};
-
 
 const getUserFromToken = async function(token: string) {
   const response = await axios.get(`${baseUrl}/from-token`, {
@@ -37,34 +16,6 @@ const getUserFromToken = async function(token: string) {
   return foundUser;
 };
 
-const updateInfo = async function(userName: string, email: string) {
-  const token = getToken();
-  const response = await axios.put(`${baseUrl}/update`, {
-    userName, email,
-  }, {
-    headers: { Authorization: `bearer ${token}` },
-  }).then((res) => res)
-    .catch((error) => {
-      const { message } = error.response.data.error;
-      return message;
-    });
-
-  return response.data;
-};
-
-const updatePassword = async function(currentPassword: string, newPassword: string) {
-  const token = getToken();
-  const response = await axios.put(`${baseUrl}/change-password`, {
-    currentPassword, newPassword,
-  }, {
-    headers: { Authorization: `bearer ${token}` },
-  }).then((res) => res)
-    .catch((error) => {
-      const { message } = error.response.data.error;
-      return message;
-    });
-  return response;
-};
 
 const confirmPassword = async function(password: string) {
   const token = getToken();
@@ -82,6 +33,60 @@ const confirmPassword = async function(password: string) {
   const { match } = response.data;
   return match === 'true';
 };
+
+
+const addUser = async function(newUser: User) {
+  const response = await axios.post(`${baseUrl}/`, newUser)
+    .then((res) => res)
+    .catch((error) => {
+      const { message } = error.response.data.error;
+      return message;
+    });
+  return response;
+};
+
+
+const updateInfo = async function(userName: string, email: string) {
+  const token = getToken();
+  const response = await axios.put(`${baseUrl}/update-info`, {
+    userName, email,
+  }, {
+    headers: { Authorization: `bearer ${token}` },
+  }).then((res) => res)
+    .catch((error) => {
+      const { message } = error.response.data.error;
+      return message;
+    });
+
+  return response.data;
+};
+
+
+const updatePassword = async function(currentPassword: string, newPassword: string) {
+  const token = getToken();
+  const response = await axios.put(`${baseUrl}/change-password`, {
+    currentPassword, newPassword,
+  }, {
+    headers: { Authorization: `bearer ${token}` },
+  }).then((res) => res)
+    .catch((error) => {
+      const { message } = error.response.data.error;
+      return message;
+    });
+  return response;
+};
+
+
+const setUserLanguages = async function(knownLanguageId: string, learnLanguageId: string) {
+  const token = getToken();
+
+  const response = await axios.put(`${baseUrl}/set-languages`, { knownLanguageId, learnLanguageId }, {
+    headers: { Authorization: `bearer ${token}` },
+  });
+
+  return response.data;
+};
+
 
 const removeUser = async function() {
   const token = getToken();
