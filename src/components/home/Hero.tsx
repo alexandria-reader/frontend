e87799b4cbd5315b018500a27c59cecea1143a82
Eamon <eamon0989@gmail.com>
2022-01-27 17:17:55 +0100

    Fixed text clipping on heading, fixed contrast of demo button for accessability

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -10,8 +10,8 @@ export default function Benefits() {
           <div className="items-center h-full flex flex-col md:flex-row ">
             {/* Section header */}
             <div className=" flex-1 flex justify-between h-max flex-col gap-6">
-              <h1 className={`${window.innerHeight < 800 ? 'text-4xl' : 'text-5xl'} text-center md:text-left xl:text-6xl lg:text-5xl md:text-4xl sm:text-6xl font-extrabold leading-tighter tracking-normal`} data-aos="zoom-y-out">Alexandria:<br />Learn languages by<br />
-                <span className="bg-clip-text block text-transparent bg-gradient-to-r from-[#167cbd] to-[#6a007b]">reading what you enjoy</span>
+              <h1 className={`${window.innerHeight < 800 ? 'text-4xl' : 'text-5xl'} text-center  overflow-y-clip md:text-left xl:text-6xl lg:text-5xl md:text-4xl sm:text-6xl font-extrabold leading-tighter tracking-normal`} data-aos="zoom-y-out">Alexandria:<br />Learn languages by<br />
+                <span className="bg-clip-text block pb-1 text-transparent bg-gradient-to-r from-[#167cbd] to-[#6a007b]">reading what you enjoy</span>
               </h1>
               <div className="max-w-3xl">
                 <p className="text-xl text-center md:text-left text-secondary tracking-normal" data-aos="zoom-y-out" data-aos-delay="150">Bring your texts. Translate words. Review.</p>
@@ -21,7 +21,7 @@ export default function Benefits() {
                   <a href="/signup"><button className="focus:outline-none lg:w-44 w-32 whitespace-nowrap focus:ring-2 focus:ring-offset-2 focus:ring-fuchsia-700 bg-fuchsia-900 transition duration-150 ease-in-out hover:bg-fuchsia-700 lg:text-xl lg:font-bold  rounded text-white border border-fuchsia-900 py-2 sm:py-4 text-md">Sign up</button></a>
                 </div>
                 <div>
-                  <a href="demo"><button id='open-demo-btn' className="focus:outline-none w-32 lg:w-44 whitespace-nowrap focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 bg-transparent transition duration-150 ease-in-out hover:border-sky-500 lg:text-xl lg:font-bold  hover:text-sky-500 rounded border border-sky-600 text-sky-700 py-2 sm:py-4 text-md">Demo</button></a>
+                  <a href="demo"><button id='open-demo-btn' className="focus:outline-none w-32 lg:w-44 whitespace-nowrap focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 bg-transparent transition duration-150 ease-in-out hover:border-sky-500 lg:text-xl lg:font-bold  hover:text-sky-500 rounded border border-sky-700 dark:border-sky-600 text-sky-700/90 dark:text-sky-600 py-2 sm:py-4 text-md">Demo</button></a>
                 </div>
               </div>
           </div>
